94915b524abf9d02bba8948a9c4cb34107d8dd27
Daniel Ceriana <60513314+Daniel-Ceriana@users.noreply.github.com>
2025-09-26 23:22:33 -0300

    [FIX]Update userReducer.js
    Para arreglar problema con carrito al iniciar sesion

--- src/redux/reducers/userReducer.js
+++ src/redux/reducers/userReducer.js
@@ -12,26 +12,37 @@ const initialState = {
 const userReducer = createReducer(initialState, (builder) => {
   return builder
     .addCase(signIn.fulfilled, (state, action) => {
-      const newStore = { ...state, loading: false, user: action.payload.user };
-      if (action.payload.user.cart.length == 0 && action.payload.cart.length!=0) {
+      if (!action.payload.user) {
+        // si no hay user, no toco nada
+        return { ...state, loading: false };
+      }
+      const newStore = {
+        ...state,
+        loading: false,
+        user: action.payload.user,
+      };
+      if (
+        action.payload.user.cart.length == 0 &&
+        action.payload.cart.length != 0
+      ) {
         //si el usuario que vuelve de la bse de datos no tiene carrito
         newStore.user.cart = action.payload.cart; //le agrego la carta que tengo en el store
       }
-      
 
       return newStore;
     })
-    .addCase(signIn.pending, (state, action) => {
-      const newStore = { ...state, loading: true, user: action.payload };
-      return newStore;
+    .addCase(signIn.pending, (state) => {
+      return { ...state, loading: true };
+    })
+    .addCase(signIn.rejected, (state, action) => {
+      return { ...state, loading: false, error: action.payload };
     })
     .addCase(signUp.fulfilled, (state, action) => {
       const newStore = { ...state };
       return newStore;
     })
-    .addCase(signOut, (state, action) => {
-      const newStore = { ...state, user: action.payload };
-      return newStore;
+    .addCase(signOut, (state) => {
+      return { ...state, user: undefined };
     })
     .addCase(verifyToken.fulfilled, (state, action) => {
       const newStore = { ...state, user: action.payload };
